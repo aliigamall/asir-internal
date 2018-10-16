ed14b2d038d8de11f0024d6776d5e80700b23928
unknown <Administrator@as-agamal.amana-as.gov>
2018-10-16 10:35:39 +0300

    add home page

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,35 +1,70 @@
-// document.querySelector('.main-header__responsive-icon').addEventListener('click', function(){
-//     document.querySelector('.main-header').classList.toggle('main-header--open');
-// });
-
-// document.querySelector('.main-header__overlay').addEventListener('click', function(){
-//     document.querySelector('.main-header').classList.toggle('main-header--open');
-// });
-
-// fab button
-const fabComponent = document.querySelector('.fab');
-const fabButton = document.querySelector('.fab__trigger');
-
-if (fabComponent) {
-    fabButton.addEventListener('click', () => {
-        fabComponent.classList.toggle('is-opened');
-    });
-};
 
 $(function () {
-    // tabs
-    var tabsComponent = $('.tab-bar');
-    if (tabsComponent) {
-        $('.tab-bar__link').on('click', function (event) {
-            event.preventDefault();
-            var tabTarget = this.hash;
-            $('.tab-bar__item').removeClass('tab-bar__item--active');
-            $(this).parent().addClass('tab-bar__item--active');
+    // var IsEditMode = document.forms[MSOWebPartPageFormName].MSOLayout_InDesignMode.value;
+    // if (IsEditMode == "1") {
+	// 	$('html').addClass('is-editMode');
+	// }
 
-            $('.tab-item').removeClass('tab-item--active');
-            $(tabTarget).addClass('tab-item--active');
-        });
-    }
+	// if (IsEditMode != "1") {
+	// 	$('html').addClass('is-displayMode');
+    // }
+
+    // ================ search box   
+	var searchButton =  $('.search-box__icon');
+	var searchBox =  $('.search-box');
+	var searchTextBox =  $('.search-box__input');
+
+
+	searchButton.on('click', openSearchBox); 
+
+	function openSearchBox(){
+		searchBox.addClass('is-open');
+		searchTextBox.focus();
+		searchTextBox.val("");
+	}
+
+	var searchPage = "/ar/Pages/Search.aspx?k=";
+
+	searchTextBox.on("change keyup paste mouseup", function(){
+		var searchKeyword = searchTextBox.val();	
+		var concatText = searchPage + searchKeyword;
+		
+		$(document).keypress(function (event) {
+			if (event.which == 13) {
+				event.preventDefault();
+				if(searchKeyword != ""){
+					window.location.href = concatText ;	    		
+				}
+			}
+		});
+	});
+	
+	$(document).mouseup(function (e) {
+		if (!searchBox.is(e.target) 
+			&& searchBox.has(e.target).length === 0) 
+		{
+			searchBox.removeClass('is-open');
+		}
+	});
+    // ================ /search box 
+
+    // ================ global menu
+    $('#mainMenuBtn, .global-menu__block .global-menu__close').on('click', function (event) {
+		event.preventDefault();
+		$('.global-menu__block').toggleClass('is-shown');
+		$('body').toggleClass('u-overflow-hidden');
+	});
+
+	$(document).keyup(function (e) {
+		if (e.keyCode == 27) {
+			$('.global-menu__block').removeClass('is-shown');
+			$('body').removeClass('u-overflow-hidden');
+			if ($(".dialog").length > 0)
+				$(".dialog").removeClass("is-shown");
+
+		}
+	});
+	// ================ /global menu  
 });
 
 
